fdae718c69e3b8fd7cf5f55b743bd75b4e0a9260
Tolben <vitek.pasek@gmail.com>
2025-10-22 21:01:08 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/030_foreign_keys.sql
+++ schema/030_foreign_keys.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 (map@1e83bb6)
+-- Auto-generated from schema-map.psd1 (map@6cefe8e)
 -- table: verify_events
 ALTER TABLE verify_events ADD CONSTRAINT fk_verify_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL;
